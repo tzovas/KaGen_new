0bfac3d574d221efbac9836dcc81bcfa348bef28
tzovas <tzovas.charilaos@hu-berlin.de>
2020-03-10 14:10:36 +0100

    version of GatherPrint that prints 2d coordinates

--- include/io/generator_io.h
+++ include/io/generator_io.h
@@ -67,7 +67,7 @@ class GeneratorIO {
     local_num_edges_++;
   }
 
-/*
+  //TODO: is this needed? if not, remove
   inline void PushEdge(SInt v1, SInt v2){
 	if( v1 < v2){
 		edges_.emplace_back( std::make_tuple( v1, v2) );
@@ -77,8 +77,7 @@ class GeneratorIO {
 	//v1 > v2 ? edges_.emplace_back( std::make_tuple( v1, v2) ) : edges_.emplace_back( std::make_tuple( v2, v1) );
     local_num_edges_++;
   }
-  */
-  
+
   void OutputEdges() const { 
 #ifdef SINGLE_LIST
     GatherPrint(identity<Edge>());
@@ -104,10 +103,133 @@ class GeneratorIO {
     throw std::logic_error("Not implemented, should not be called.");
   }
 
-  void GatherPrint(identity<std::tuple<LPFloat, LPFloat, SInt>>) const {
-    throw std::logic_error("Not implemented, should not be called.");
+  typedef std::tuple<LPFloat, LPFloat> coord_2d;
+  typedef std::tuple<LPFloat, LPFloat, LPFloat> coord_3d;
+
+
+  //template<typename coord_dd>
+  int GatherCoords( identity<std::tuple<LPFloat, LPFloat, SInt>>, std::vector<std::tuple<LPFloat, LPFloat>> &globalCoords, std::vector<SInt> &globalIds) const {
+    // Exchange local dist
+    PEID rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    //cannot remove duplicates locally as function is const  and cannot change edges_
+
+    //break vector of tuples into two vectors
+    unsigned int lSize = NumEdges();
+    std::vector<std::tuple<LPFloat, LPFloat>> localCoords(lSize);
+    std::vector<SInt> localIds(lSize);
+    assert( lSize==edges_.size() );
+
+    for ( unsigned int i=0; i<lSize; i++ ){
+        auto edge = edges_[i];
+        localCoords[i] = std::tuple<LPFloat, LPFloat>( std::get<0>(edge) , std::get<1>(edge) );
+        localIds[i] = std::get<2>(edge);
+    }
+
+    int total_num_edges = 0;
+    
+    // Gather number of edges for each PE
+    std::vector<int> displ(size);
+    std::vector<int> num_edges(size);
+
+    MPI_Gather(&lSize, 1, MPI_INT,
+               num_edges.data(), 1, MPI_INT, 
+               ROOT, MPI_COMM_WORLD);
+    int current_displ = 0;
+    if (rank == ROOT) {
+      for (SInt i = 0; i < num_edges.size(); ++i) {
+        displ[i] = current_displ;
+        total_num_edges += num_edges[i];
+        current_displ = total_num_edges;
+      }
+    }
+
+    std::cout<< __FILE__ << ", " << __LINE__ << ": " << rank << ", local size= " << lSize <<  ", total_num_edges= " << total_num_edges << std::endl;
+    globalCoords.resize(total_num_edges);
+    globalIds.resize(total_num_edges);
+
+    // Gather actual coordinates
+    MPI_Datatype MPI_COORD;
+    MPI_Type_vector(1, 2, 0, MPI_DOUBLE, &MPI_COORD);
+    MPI_Type_commit(&MPI_COORD);
+    
+    MPI_Gatherv(localCoords.data(), lSize, MPI_COORD,
+                globalCoords.data(), num_edges.data(), displ.data(), MPI_COORD, 
+                ROOT, MPI_COMM_WORLD);  
+
+    // Gather actual vertex indices
+    MPI_Gatherv(localIds.data(), lSize, MPI_LONG,
+                globalIds.data(), num_edges.data(), displ.data(), MPI_LONG, 
+                ROOT, MPI_COMM_WORLD);
+
+    assert( globalCoords.size()==globalIds.size() );
+
+    return total_num_edges;
   }
 
+
+  //2D point output
+  void GatherPrint(identity<std::tuple<LPFloat, LPFloat, SInt>>) const {
+    PEID rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    std::vector<std::tuple<LPFloat, LPFloat>> globalCoords; 
+    std::vector<SInt> globalIds; 
+
+    GatherCoords( identity<Edge>(), globalCoords, globalIds);
+
+    if (rank == ROOT) {
+        //checks
+        if( not std::is_sorted(globalIds.begin(), globalIds.end()) ){
+            //throw std::logic_error("ERROR: For some reason, the coordinate indices are not sorted.");
+            std::cout<< "WARNING: indices are not sorted, will sort now" << std::endl;
+
+            //zip again to sort and remove duplicates
+            std::vector<std::tuple<LPFloat, LPFloat, SInt>> forSort;
+            for( unsigned long i=0; i<globalCoords.size(); i++){
+                forSort.push_back( std::tuple<LPFloat, LPFloat, SInt>( std::get<0>(globalCoords[i]), std::get<1>(globalCoords[i]), globalIds[i] ) );
+            }
+
+            std::stable_sort( forSort.begin(), forSort.end(), []( auto x1, auto x2){
+                return std::get<2>(x1) < std::get<2>(x2);
+                });
+            
+            std::cout<< __FILE__ << ", " << __LINE__ << ": before removing duplicates: " << forSort.size() << std::endl;
+            forSort.erase( unique(forSort.begin(), forSort.end()), forSort.end() );
+            std::cout<< __FILE__ << ", " << __LINE__ << ": after: " << forSort.size() << std::endl;
+
+            //unzip
+            SInt newSize = forSort.size();
+            globalIds.resize( newSize, 0);
+            globalCoords.resize( newSize,std::tuple<LPFloat, LPFloat>(0.0, 0.0) );
+            for ( unsigned int i=0; i<newSize; i++ ){
+              auto edge = forSort[i];
+              globalCoords[i] = std::tuple<LPFloat, LPFloat>( std::get<0>(edge) , std::get<1>(edge) );
+              globalIds[i] = std::get<2>(edge);
+            }
+        }
+
+      for( unsigned int i=0; i<globalIds.size()-1; i++){
+        if( globalIds[i]==globalIds[i+1]){
+          throw std::logic_error("ERROR: vertex id " + std::to_string(globalIds[i]) + " is duplicated.");
+        }
+      }
+
+      // Output edges
+      FILE* fout = fopen(config_.coord_file.c_str(), "w+");
+
+      for (auto coord : globalCoords){
+        fprintf(fout, "%f %f\n", std::get<0>(coord) , std::get<1>(coord) );
+      }
+      fclose(fout);
+    }
+      
+  }//GatherPrint, 2D
+
+  //for the edges
   void GatherPrint(identity<std::tuple<SInt, SInt>>) const {
     // Exchange local dist
     PEID rank, size;
@@ -140,22 +262,25 @@ class GeneratorIO {
                 edges.data(), num_edges.data(), displ.data(), MPI_EDGE, 
                 ROOT, MPI_COMM_WORLD);
 
-
     if (rank == ROOT) {
       // Sort edges and remove duplicates
       std::sort(std::begin(edges), std::end(edges));
-      SInt total_edges = edges.size();
+      //SInt total_edges = edges.size();
       edges.erase(unique(edges.begin(), edges.end()), edges.end());
       
       // Output edges
       FILE* fout = fopen(config_.output_file.c_str(), "w+");
 #ifndef OMIT_HEADER
-      fprintf(fout, "%% %llu %lu\n", config_.n, edges.size());
+      fprintf(fout, "%llu %lu\n", config_.n, edges.size());
 #endif
       for (auto edge : edges) fprintf(fout, "%llu %llu\n", std::get<0>(edge) , std::get<1>(edge) );
       fclose(fout);
     }
   }
+
+  //
+  // distributed versions
+  //
 
   // node id output
   void Print(identity<std::tuple<SInt, SInt>>) const {
